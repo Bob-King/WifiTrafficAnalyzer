e7af37b211b12924540d9cba212f781dc9b2feb9
Xingyou <xingyou.lj@alibaba-inc.com>
2017-07-13 13:42:23 +0800

    Remove unnecessary imports

--- analyzer/src/org/mars/kjli/analyzer/MainApplet.java
+++ analyzer/src/org/mars/kjli/analyzer/MainApplet.java
@@ -1,7 +1,6 @@
 package org.mars.kjli.analyzer;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
